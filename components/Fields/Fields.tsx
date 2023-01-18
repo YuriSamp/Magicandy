06d924ce57197082dbbe8b404c324708a7310a49
RamosCarlosEduardo <carlosramos.engenharia@gmail.com>
2023-01-17 21:52:52 -0300

    inserting form fields component

---
+++ components/Fields/Fields.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface IProps {
+    Name : string
+    Label : string
+    Type: string
+}
+
+function Fields({Name, Label, Type}: IProps) {
+    
+
+    return (
+    <div className='flex flex-col gap-2'>
+        <label htmlFor={Name}>{Label}</label>
+        {
+            Type != "textarea"
+            ? <input type={Type} id={Name} className='py-2 px-4 rounded-lg  ring-backgroundPink ring-2 bg-slate-200' />
+            : <textarea id={Name} className='py-2 px-4 rounded-lg resize-none h-40 ring-backgroundPink ring-2 bg-slate-200' />
+        } 
+    <span></span>
+  </div>
+  )
+}
+
+export default Fields
--- pages/Contato.tsx
+++ pages/Contato.tsx
@@ -3,6 +3,7 @@ import Footer from '../components/Footer/Footer'
 import Navbar from '../components/Navbar/Navbar'
 import { useForm, SubmitHandler } from "react-hook-form";
 import Button from '../components/Button/Button';
+import Fields from '../components/Fields/Fields';
 
 //TODO Criar um componente botao
 
@@ -15,52 +16,54 @@ type Inputs = {
 };
 
 function Contato() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+  const onSubmitHandler = () => {};
 
   return (
     <>
       <Navbar />
       <main className='flex flex-col min-h-[80vh] px-4 py-4 items-center justify-center w-full min-w-[320px]  bg-backgroundPink' >
-        <form className='flex flex-col gap-4 w-full max-w-[30rem] bg-backgroundWhite p-10 rounded-lg border-2 border-fontPurple' onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className='flex flex-col gap-4 w-full max-w-[30rem] bg-backgroundWhite p-10 rounded-lg border-2 border-fontPurple'
+          onSubmit={onSubmitHandler}
+        >
           <div className='pt-4 text-center'>
             <h1 className='text-2xl font-kalam text-fontPurple font-semibold'>Entre em contato conosco</h1>
           </div>
 
-          <div className='flex flex-col space-y-1'>
-            <label htmlFor='name'>Nome</label>
-            <input type='text' id='name' className='py-2 px-4 rounded-lg  ring-backgroundPink ring-2 bg-slate-200' {...register("Nome")} />
-            <span></span>
-          </div>
+          <Fields 
+            Name="name"
+            Label="Nome"
+            Type="text"
+          />
 
-          <div className='flex flex-col space-y-1'>
-            <label htmlFor='email'>Email</label>
-            <input type='text' id='email' className='py-2 px-4 rounded-lg ring-backgroundPink ring-2 bg-slate-200'  {...register("Email")} />
-            <span></span>
-          </div>
+          <Fields 
+            Name="email"
+            Label="E-mail"
+            Type="email"
+          />
 
-          <div className='flex flex-col space-y-1'>
-            <label htmlFor='telephone'>Telefone</label>
-            <input type='text' id='telephone' className='py-2 px-4 rounded-lg ring-backgroundPink ring-2 bg-slate-200' {...register("Telefone")} />
-            <span></span>
-          </div>
+          <Fields
+            Name="telephone"
+            Label="Telefone"
+            Type="tel"
+          />
 
-          <div className='flex flex-col space-y-1'>
+          <div className='flex flex-col gap-2'>
             <label htmlFor='contactReason'>Motivo do contato</label>
-            <select id='contactReason' className='py-2 px-4 rounded-lg ring-backgroundPink ring-2 bg-slate-200 ' {...register("MotivoDoContato")}>
-              <option >Duvida</option>
+            <select id='contactReason' className='py-2 px-4 rounded-lg ring-backgroundPink ring-2 bg-slate-200 '>
+              <option>Duvida</option>
               <option>Sugestão</option>
               <option>Reclamação</option>
               <option>Outros</option>
             </select>
             <span></span>
           </div>
-
-          <div className='flex flex-col'>
-            <label htmlFor='text'>Mensagem</label>
-            <textarea id='text' className='py-2 px-4 rounded-lg resize-none h-40 ring-backgroundPink ring-2 bg-slate-200' {...register("texto")} />
-            <span></span>
-          </div>
+          
+          <Fields 
+            Name="textarea"
+            Label="Mensagem"
+            Type="textarea"
+          />
 
           <div className='flex justify-center pt-4'>
             <Button value={'Enviar Formulario'} />
