0ef397a768385bf05609198d55c4c9c7074545f2
YuriSamp <yurisamp123@gmail.com>
2023-01-23 20:34:48 -0300

    trying to connect to database 2x

--- services/connect.js
+++ services/connect.js
@@ -2,31 +2,38 @@ import mongoose, { mongo } from 'mongoose';
 
 //? acho que a tipagem seria isConnected = boolean
 
-const connection = {};
+// const connection = {};
 
 async function connect() {
-  if (connection.isConnected) {
-    return;
+  // if (connection.isConnected) {
+  //   return;
+  // }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log(error);
   }
-  if (mongoose.connect.length > 0) {
-    connection.isConnected = mongoose.connections[0].readyState;
-    if (mongoose.connect.length === 1) {
-      return;
-    }
-    mongoose.disconnect;
-  }
-  mongoose.set('strictQuery', false);
-  const db = mongoose.connect(process.env.MONGODB_URI);
-  connection.isConnected = db.connections[0].readyState;
+
+  // if (mongoose.connect.length > 0) {
+  //   connection.isConnected = mongoose.connections[0].readyState;
+  //   if (mongoose.connect.length === 1) {
+  //     return;
+  //   }
+  //   mongoose.disconnect;
+  // }
+  // mongoose.set('strictQuery', false);
+  // const db = mongoose.connect(process.env.MONGODB_URI);
+  // connection.isConnected = db.connections[0].readyState;
 }
 
 async function disconnect() {
-  if (connection.isConnected) {
-    if (process.env.NODE_ENV === 'production') {
-      await mongo.disconnect();
-      connect.isConnected = false;
-    }
-  }
+  // if (connection.isConnected) {
+  // if (process.env.NODE_ENV === 'production') {
+  // await mongo.disconnect();
+  // connect.isConnected = false;
+  // }
+  // }
 }
 
 const dbConnect = { connect, disconnect };
