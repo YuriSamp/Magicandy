e6c6ce03c51b24fbe7ee5b06b991e46c8b06c681
RamosCarlosEduardo <carlosramos.engenharia@gmail.com>
2023-01-25 16:40:06 -0300

    styling footer, page 404 and nav effects

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,26 +1,32 @@
 import Link from 'next/link'
 import { BsGithub, BsLinkedin } from 'react-icons/bs'
-import { RxDividerVertical } from 'react-icons/rx'
 
 const Footer = () => {
     return (
         <>
-            <footer className="flex flex-col items-center border-t-2 py-10 gap-5 bg-[#f8f8ff]">
-                <span className="text-2xl text-fontPurple text-center">Copyright © 2023 <br className='md:hidden' /> All rights reserved.</span>
-                <div className='flex flex-col md:flex-row gap-2'>
-                    <span className='flex text-2xl  text-center items-center gap-2'>
-                        <Link href='https://github.com/YuriSamp'><BsGithub className='text-violet-900' /></Link>
-                        <Link href='https://www.linkedin.com/in/yurisamp/'><BsLinkedin className='text-violet-900' /></Link>
-                        <p className='text-fontPurple'>Yuri Sampaio </p>
-                        <RxDividerVertical className='text-4xl text-fontPurple hidden md:flex' />
+            <footer className="flex flex-col items-center gap-2 text-fontPurple text-3xl border-t-2 pt-10 bg-[#f8f8ff]">
+                <h3 className="font-bold">Magicandy</h3>
+                <p className="text-xl text-black">Projeto e designer desenvolvidos por:</p>
+                <div className='flex flex-col pb-10 md:flex-row gap-10'>
+                    
+                    <span className='flex flex-col text-2xl  text-center items-center gap-2'>
+                        <p className='text-black font-semibold tracking-wider'>Yuri</p>
+                        <span className="flex gap-4">
+                            <Link href='https://github.com/YuriSamp'><BsGithub className='text-black' /></Link>
+                            <Link href='https://www.linkedin.com/in/yurisamp/'><BsLinkedin className='text-black' /></Link>
+                        </span>
                     </span>
-                    <span className='flex text-2xl text-center items-center gap-2'>
-                        <Link href='https://github.com/RamosCarlosEduardo'><BsGithub className='text-violet-900' /></Link>
-                        <Link href='https://www.linkedin.com/in/carlos-eduardo-ramos-92b524218/'><BsLinkedin className='text-violet-900' /></Link>
-                        <p className='text-fontPurple'>Carlos Ramos </p>
+                        
+                    <span className='flex flex-col text-2xl pl-10 text-center items-center gap-2 border-l-4 border-fontPurple'>
+                        <p className='text-black font-semibold tracking-wider'>Carlos</p>
+                        <span className="flex gap-4">
+                            <Link href='https://github.com/RamosCarlosEduardo'><BsGithub className='text-black' /></Link>
+                            <Link href='https://www.linkedin.com/in/carlos-eduardo-ramos-92b524218/'><BsLinkedin className='text-black' /></Link>
+                        </span>
                     </span>
-                </div>
 
+                </div>
+                <span className="w-full text-lg text-fontPurple py-3 bg-gray-50 text-center">Copyright © 2023 <br className='md:hidden' /> All rights reserved.</span>
             </footer>
         </>
     )
--- pages/404.tsx
+++ pages/404.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import NotFound from 'public/page404.png'
 import { AiOutlineArrowLeft } from 'react-icons/ai'
+import Link from 'next/link'
 
 //TODO Revisar
 
@@ -17,16 +18,20 @@ export default function Custom404() {
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <main className='bg-backgroundPink min-h-[calc(100vh-316px)] flex justify-center items-center'>
-        <div className='px-48 flex items-center gap-32'>
-          <div className='flex flex-col items-center '>
+        <div className='bg-backgroundWhite flex items-center border-2 border-fontPurple rounded-lg'>
+          <div className='flex flex-col items-center px-20'>
             <p className='font-kalam text-2xl'>Não achou o que estava procurando?</p>
             <div className='flex items-center'>
-              <AiOutlineArrowLeft />
-              <Button value='Voltar' type='LINK' href='../' />
+              <Link 
+                className='flex items-center justify-center gap-2 cursor-pointer text-lg p-2  mx-auto radius-5 ring-2 my-[1rem] ring-backgroundPink font-kalam rounded-lg select-none'
+                href='../produtos'
+              >
+                <AiOutlineArrowLeft />Ver produtos
+              </Link>
             </div>
           </div>
           <div className='flex flex-col flex-wrap-reverse'>
-            <Image src={NotFound} alt='foto de garotos olhando atráves de donuts' className='rounded-lg' width={600} height={400} />
+            <Image src={NotFound} alt='foto de garotos olhando atráves de donuts' className='rounded-r-lg' width={600} height={400} />
           </div>
         </div>
       </main>
