b1aa61d47c2aaaa2c3c18acc94c60e7f8e686be0
YuriSamp <yurisamp123@gmail.com>
2023-02-02 14:35:27 -0300

    change banner to a video

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home({ db }: DataBase) {
   useEffect(() => {
     setDomLoaded(true);
   }, []);
-  
+
   return (
     <>
       <Head>
@@ -41,36 +41,35 @@ export default function Home({ db }: DataBase) {
           '>A magia da felicidade <br className='hidden md:inline-block' />
             <span className='md:ml-32'>para adoçar seu dia!</span> </h2>
         </div> */}
-        
-		<div className="h-[calc(100vh_-_146px)] w-screen overflow-hidden relative flex items-center justify-center">
-			<div className="opacity-90 w-full h-full">
-			{domLoaded && (
-			<video 
-				className="object-fill h-full w-full overflow-hidden"
-				autoPlay
-				loop
-				muted
-				preload="auto"
-				playsInline>
-				<source src="/magicandy.mp4" type='video/mp4' />
-			</video>
-			)}
-			</div>
-			<div className="flex flex-col justify-center items-center gap-6 max-w-[900px] text-backgroundWhite font-kalam absolute backdrop-blur-sm bg-black/30 border-none rounded-xl p-8">
-				<div className='flex items-center justify-center gap-6'>
-					<Image
-						className='w-[80px]'
-						src={unicornImg}
-						width={45}
-						height={45}
-						alt=""
-					/>
-					<h2 className='text-6xl'>Magicandy</h2>
-				</div>
-				<p className='text-2xl text-justify teste'>Criada em 2023, a partir de um sonho e um compromisso, de fazer nossos produtos com amor e qualidade, para que em cada fatia, cada mordida e cada suspiro, possamos compartilhar com vocês a magia da felicidade. </p>
-			</div>
-		</div>
-        
+
+        <div className="h-[calc(100vh_-_146px)] w-screen overflow-hidden relative flex items-center justify-center">
+          <div className="opacity-90 w-full h-full">
+            {domLoaded && (
+              <video
+                className="object-fill h-full w-full overflow-hidden"
+                autoPlay
+                loop
+                muted
+                preload="auto"
+                playsInline>
+                <source src="/magicandy.mp4" type='video/mp4' />
+              </video>
+            )}
+          </div>
+          <div className="flex flex-col justify-center items-center gap-6 max-w-[900px] text-backgroundWhite font-kalam absolute backdrop-blur-sm bg-black/30 border-none rounded-xl p-8">
+            <div className='flex items-center justify-center gap-6'>
+              <Image
+                className='w-[80px]'
+                src={unicornImg}
+                width={45}
+                height={45}
+                alt=""
+              />
+              <h2 className='text-6xl'>Magicandy</h2>
+            </div>
+            <p className='text-2xl text-justify teste'>Criada em 2023, a partir de um sonho e um compromisso, de fazer nossos produtos com amor e qualidade, para que em cada fatia, cada mordida e cada suspiro, possamos compartilhar com vocês a magia da felicidade. </p>
+          </div>
+        </div>
 
         <section className="flex grow text-black flex-col h-full gap-x-42 lg:flex-row lg:px-48 gap-16 md:gap-8 px-2 py-16 border-b-2 border-[#8c52ff] items-center justify-evenly flex-wrap">
           {
